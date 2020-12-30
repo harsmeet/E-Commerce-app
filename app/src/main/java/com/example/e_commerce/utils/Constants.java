c6dd6e99b43a56e182542824f2b6b64f1216545a
Ahmed <mido.zezo545@gmail.com>
2020-12-30 20:12:09 +0200

    Update libraries

--- app/src/main/java/com/example/e_commerce/utils/Constants.java
+++ app/src/main/java/com/example/e_commerce/utils/Constants.java
@@ -1,11 +1,10 @@
 package com.example.e_commerce.utils;
 
-
 public class Constants {
 
     // Keys for every request to server
-    public static final String CONSUMER_KEY = "ck_33e5438f365cfdce74b96104330823a3c8f11eac";
-    public static final String SECRET_KEY = "cs_a25798bfab1a7aa6207717fc07034e79c77f5cf9";
+    public static final String CONSUMER_KEY = "Your consumer key";
+    public static final String SECRET_KEY = "Your secret key";
 
     // For signup
     public static final String SUCCESS = "Registered Successfully";
